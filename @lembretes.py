647fcaece3989530e6ddcee68467c59f92b37713
luksadmin <lukszwei2@gmail.com>
2021-01-18 22:13:42 -0300

    Files added and edited.

---
+++ @lembretes.py
@@ -0,0 +1,12 @@
+
+
+"""
+Módulo: @lembretes.py
+
+Objetivo: registrar lembretes de coisas que é recomendável fazer
+"""
+
+def pendentes():
+    """
+    1 - Fazer registro do método .get_login() [ from os import getlogin ]
+    """
