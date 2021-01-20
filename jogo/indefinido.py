b93cf66bf11118027908c765d638976a7703dc16
luksadmin <lukszwei2@gmail.com>
2021-01-20 20:56:03 -0300

    Files added and edited.

---
+++ jogo/indefinido.py
@@ -0,0 +1,22 @@
+
+
+"""
+Módulo: indefinido.py
+"""
+
+def fonte():
+    """
+    Curso: || Programação em Python do básico ao avançado
+    Seção: || Seção 25:Projeto Python 1 - Game
+    Aula:  || 178. Criando nosso projeto
+    """
+
+# Criando projeto e arquivos necessários
+def parte1():
+    """
+    1 - Criação de um projeto
+    2 - [ raiz - botão direito - new - python package - models ]
+    3 - [ models - botão direito - new - python file - calcular ]
+    4 - [ raiz - botão direito - new - python file - teste ]
+    5 - [ raiz - botão direito - new - python file - game ]
+    """
